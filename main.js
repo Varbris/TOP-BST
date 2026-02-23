5fe5c1fada7c05498742f6bd48e12247fa0a2f49
varbris <92345143+Varbris@users.noreply.github.com>
2026-02-23 11:43:23 +0700

    fix: build tree method
    when the mid is greater than array length,
    will return null instead of undefined

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 class Node {
-  constructor(value, nextNode = null) {
+  constructor(value) {
     this.data = value;
-    this.left;
-    this.right;
+    this.left = null;
+    this.right = null;
   }
 }
 
@@ -16,15 +16,17 @@ class Tree {
     if (start > end) return null;
 
     let mid = start + Math.floor((end - start) / 2);
-    if (mid > arr.length - 1) {
-      return;
-    }
+
     let root = new Node(arr[mid]);
 
     root.left = this.buildTree(arr, start, mid - 1);
     root.right = this.buildTree(arr, mid + 1, end);
 
-    return root;
+    if (mid > arr.length) {
+      return null;
+    } else {
+      return root;
+    }
   }
 
   merge(leftArr, rightArr) {
